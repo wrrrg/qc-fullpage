1eb30df3de4fe4535888f433bc0739aedb8021c5
wrrrg <bill.gryta@gmail.com>
2018-09-19 05:14:49 -0500

    header animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,24 @@ $(document).ready(function() {
   $("#fullpage").fullpage({
     //options here
     autoScrolling: true,
-    scrollHorizontally: true
+    scrollHorizontally: true,
+    afterRender: function() {
+      $("#header").css("opacity", 0);
+    },
+    // afterLoad: function(index, nextIndex) {
+    //   if (index !== 0) {
+    //     $("#header").css("opacity", 1);
+    //   } else if (index === 0) {
+    //     $("#header").css("opacity", 0);
+    //   }
+    // }
+    onLeave: function(index, nextIndex, direction) {
+      if (index === 0) {
+        $("#header").css("opacity", 0);
+      } else {
+        $("#header").css("opacity", 1);
+      }
+    }
   });
 
   //methods
@@ -25,4 +42,11 @@ $(document).ready(function() {
   audio.onTimeUpdate = function() {
     $("#progress").css("width", (current_time / length) * 100 + "%");
   };
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+    } else {
+      $("#header").hide();
+    }
+  });
 });
