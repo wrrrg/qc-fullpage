c93c879dd0ece467534832ff6457e6427cc79642
BillMac <bill.gryta@gmail.com>
2018-10-04 16:30:16 -0500

    new show loop

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,198 +2,200 @@
 const songkick = "io09K9l3ebJxmxe2";
 const skQuietCompanyId = "582111";
 
-eventSearch = function() {
-  $("#events-div").empty();
-  $.ajax({
-    url:
-      "https://api.songkick.com/api/3.0/artists/" +
-      skQuietCompanyId +
-      "/calendar.json?apikey=" +
-      songkick,
-    method: "GET"
-  }).then(function(res) {
-    console.log(res);
-    const result = res["resultsPage"]["results"]["event"];
-    console.log(result);
-    console.log(result.length);
+eventSearch = function () {
+	$("#events-div").empty();
+	$.ajax({
+		url: "https://api.songkick.com/api/3.0/artists/" +
+			skQuietCompanyId +
+			"/calendar.json?apikey=" +
+			songkick,
+		method: "GET"
+	}).then(function (res) {
+		console.log(res);
+		const result = res["resultsPage"]["results"]["event"];
+		console.log(result);
+		console.log(result.length);
 
-    for (i = 0; i < result.length; i++) {
-      const displayName = result[i]["venue"]["displayName"];
-      const location = result[i]["location"]["city"];
-      const date = result[i]["start"]["date"];
-      const uri = result[i]["uri"];
+		for (i = 0; i < result.length; i++) {
+			const displayName = result[i]["venue"]["displayName"];
+			const location = result[i]["location"]["city"];
+			const date = result[i]["start"]["date"];
+			const uri = result[i]["uri"];
 
-      const eventDiv = $("<div class='event-div'>");
-      const eventName = $("<span class='event-name'>").html(displayName);
-      const eventLocation = $("<span class='event-location'>").html(location);
-      const spacer = $("<span class='spacer'>").html(" - ");
-      const eventStart = $("<span class='event-start'>").html(date);
-      const eventUri = $("<span class='event-uri'>").html(
-        "<a href=" + uri + ">Tickets</a>"
-      );
+			const eventDiv = $("<div class='event-div'>");
+			const eventName = $("<span class='event-name'>").html(displayName);
+			const eventLocation = $("<span class='event-location'>").html(location);
+			const spacer = $("<span class='spacer'>").html(" - ");
+			const eventStart = $("<span class='event-start'>").html(date);
+			const eventUri = $("<span class='event-uri'>").html(
+				"<a href=" + uri + ">Tickets</a>"
+			);
 
-      eventDiv.append(
-        eventStart,
-        // "<span class='spacer'> - </span>",
-        eventName,
-        // "<span class='spacer'> - </span>",
-        eventLocation,
-        // spacer,
-        eventUri
-      );
+			eventDiv.append(
+				eventStart,
+				// "<span class='spacer'> - </span>",
+				eventName,
+				// "<span class='spacer'> - </span>",
+				eventLocation,
+				// spacer,
+				eventUri
+			);
 
-      $("#events-div").append(eventDiv);
-      console.log(`${displayName} - ${location} ${date}, ${uri}`);
-    }
-  });
+			$("#events-div").append(eventDiv);
+			console.log(`${displayName} - ${location} ${date}, ${uri}`);
+		}
+	});
 };
 
-eventFromJSON = function() {
-  $("#events-div").empty();
-  for (i = 0; i < showlist.length; i++) {
-    console.log(showlist[i]);
+eventFromJSON = function () {
+	$("#event-list").empty();
+	for (i = 0; i < showlist.length; i++) {
+		console.log(showlist[i]);
 
-    const venue = showlist[i]["venue"];
-    const location = showlist[i]["city"];
-    const date = showlist[i]["date"];
-    const tickets = showlist[i]["tickets"];
-    const facebook = showlist[i]["facebook"];
+		const venue = showlist[i]["venue"];
+		const location = showlist[i]["city"];
+		const date = showlist[i]["date"];
+		const tickets = showlist[i]["tickets"];
+		const facebook = showlist[i]["facebook"];
 
-    const eventDiv = $("<div class='event-div'>");
-    const eventName = $("<span class='event-name'>").html(venue);
-    const eventLocation = $("<span class='event-location'>").html(location);
-    const spacer = $("<span class='spacer'>").html(" - ");
-    const eventStart = $("<span class='event-start'>").html(date);
-    let eventUri;
-    if (showlist[i]["freeShow"]) {
-      eventUri = $("<span class='event-uri'>").html(
-        "<a href=" + facebook + ">Free Show</a>"
-      );
-    } else {
-      eventUri = $("<span class='event-uri'>").html(
-        "<a href=" + tickets + ">Tickets</a>"
-      );
-    }
-    const eventFB = $("<span class='event-fb'>").html(
-      "<a href=" + facebook + ">FB</a>"
-    );
+		const eventName = $("<span class='event-name'>").html(venue);
+		const eventLocation = $("<span class='event-location'>").html(location);
+		console.log(eventName)
+		const spacer = $("<span class='spacer'>").html(" - ");
+		const eventStart = $("<span class='event-start'>").html(date);
 
-    eventDiv.append(
-      eventStart,
-      // "<span class='spacer'> - </span>",
-      eventName,
-      // "<span class='spacer'> - </span>",
-      eventLocation,
-      // spacer,
-      eventUri,
-      //
-      eventFB
-    );
+		let eventUri;
+		if (showlist[i]["freeShow"]) {
+			eventUri = $("<span class='event-uri'>").html(
+				"<a href=" + facebook + ">Free Show</a>"
+			);
+		} else {
+			eventUri = $("<span class='event-uri'>").html(
+				"<a href=" + tickets + ">Tickets</a>"
+			);
+		}
+		const eventFB = $("<span class='event-fb'>").html(
+			"<a href=" + facebook + ">FB</a>"
+		);
 
-    $("#events-div").append(eventDiv);
-  }
+
+		// eventLi.append(
+		// 	eventStart,
+		// 	// "<span class='spacer'> - </span>",
+		// 	eventName,
+		// 	// "<span class='spacer'> - </span>",
+		// 	eventLocation,
+		// 	// spacer,
+		// 	eventUri,
+		// 	//
+		// 	eventFB
+		// );
+
+		const eventLi = "<li class='event-li'>" + eventStart + eventName + eventLocation + eventUri + eventFB + "</li>";
+		console.log(eventLi, eventName, eventLocation, eventStart, eventUri, eventFB)
+		$('.events-list').append(eventLi)
+	}
 };
 
-$(document).ready(function() {
-  let filtered = "false";
+$(document).ready(function () {
+	let filtered = "false";
 
-  // $('.albums-only').on('click', function(){
-  // 	if (filtered ==false) {
-  // 		$('.')
-  // 	}
-  // });
+	// $('.albums-only').on('click', function(){
+	// 	if (filtered ==false) {
+	// 		$('.')
+	// 	}
+	// });
 
-  $("#fullpage").fullpage({
-    //options here
-    // anchors: [ 'landing', 'shows', 'store', 'about' ],
-    autoScrolling: false,
+	$("#fullpage").fullpage({
+		//options here
+		// anchors: [ 'landing', 'shows', 'store', 'about' ],
+		autoScrolling: false,
 
-    // scrollHorizontally: true,
-    css3: true,
-    continuousVertical: false
-    // autoScrolling: false
-    // afterRender: function() {
-    //   $("#header").css("opacity", 0);
-    // }
-    // afterLoad: function(index, nextIndex) {
-    //   if (index !== 0) {
-    //     $("#header").css("opacity", 1);
-    //   } else if (index === 0) {
-    //     $("#header").css("opacity", 0);
-    //   }
-    // }
-    // onLeave: function(index, nextIndex, direction) {
-    //   if (index === 0) {
-    //     $("#header").css("opacity", 0);
-    //   } else {
-    //     $("#header").css("opacity", 1);
-    //   }
-    // }
-  });
+		// scrollHorizontally: true,
+		css3: true,
+		continuousVertical: false
+		// autoScrolling: false
+		// afterRender: function() {
+		//   $("#header").css("opacity", 0);
+		// }
+		// afterLoad: function(index, nextIndex) {
+		//   if (index !== 0) {
+		//     $("#header").css("opacity", 1);
+		//   } else if (index === 0) {
+		//     $("#header").css("opacity", 0);
+		//   }
+		// }
+		// onLeave: function(index, nextIndex, direction) {
+		//   if (index === 0) {
+		//     $("#header").css("opacity", 0);
+		//   } else {
+		//     $("#header").css("opacity", 1);
+		//   }
+		// }
+	});
 
-  $(".store-carousel").slick({
-    slidesToShow: 3,
-    speed: 300,
-    infinite: true,
-    dots: true,
-    arrows: true,
-    adaptiveHeight: true,
-    responsive: [
-      {
-        breakpoint: 1440,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-  //methods
-  $.fn.fullpage.setAllowScrolling(true);
+	$(".store-carousel").slick({
+		slidesToShow: 3,
+		speed: 300,
+		infinite: true,
+		dots: true,
+		arrows: true,
+		adaptiveHeight: true,
+		responsive: [{
+				breakpoint: 1440,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 700,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+	//methods
+	$.fn.fullpage.setAllowScrolling(true);
 
-  var audio = $("audio")[0];
+	var audio = $("audio")[0];
 
-  $("#play-pause").on("click", function() {
-    if (audio.paused) {
-      audio.play();
-      $("#play-pause").text("Pause");
-    } else {
-      audio.pause();
-      $("#play-pause").text("Listen");
-    }
-  });
+	$("#play-pause").on("click", function () {
+		if (audio.paused) {
+			audio.play();
+			$("#play-pause").text("Pause");
+		} else {
+			audio.pause();
+			$("#play-pause").text("Listen");
+		}
+	});
 
-  var length = audio.duration;
-  var current_time = audio.currentTime;
-  audio.onTimeUpdate = function() {
-    $("#progress").css("width", (current_time / length) * 100 + "%");
-  };
+	var length = audio.duration;
+	var current_time = audio.currentTime;
+	audio.onTimeUpdate = function () {
+		$("#progress").css("width", (current_time / length) * 100 + "%");
+	};
 
-  eventFromJSON();
-  //   eventSearch();
+	eventFromJSON();
+	//   eventSearch();
 
-  // $(window).scroll(function() {
-  //   if ($(this).scrollTop() > 100) {
-  //   } else {
-  //     $("#header").hide();
-  //   }
-  // });
+	// $(window).scroll(function() {
+	//   if ($(this).scrollTop() > 100) {
+	//   } else {
+	//     $("#header").hide();
+	//   }
+	// });
 });
