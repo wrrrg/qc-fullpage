acb54cdfef774c37ff379caa1f20a8264facada5
BillMac <bill.gryta@gmail.com>
2018-10-03 22:48:44 -0500

    store links/show.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,142 +1,145 @@
 // events ajax to songkick
-const songkick = "io09K9l3ebJxmxe2";
-const skQuietCompanyId = "582111";
+const songkick = 'io09K9l3ebJxmxe2';
+const skQuietCompanyId = '582111';
 
-eventSearch = function() {
-  $("#events-div").empty();
-  $.ajax({
-    url:
-      "https://api.songkick.com/api/3.0/artists/" +
-      skQuietCompanyId +
-      "/calendar.json?apikey=" +
-      songkick,
-    method: "GET"
-  }).then(function(res) {
-    console.log(res);
-    const result = res["resultsPage"]["results"]["event"];
-    console.log(result);
-    console.log(result.length);
+eventSearch = function () {
+	$('#events-div').empty();
+	$.ajax({
+		url: 'https://api.songkick.com/api/3.0/artists/' + skQuietCompanyId + '/calendar.json?apikey=' + songkick,
+		method: 'GET'
+	}).then(function (res) {
+		console.log(res);
+		const result = res['resultsPage']['results']['event'];
+		console.log(result);
+		console.log(result.length);
 
-    for (i = 0; i < result.length; i++) {
-      const displayName = result[i]["venue"]["displayName"];
-      const location = result[i]["location"]["city"];
-      const date = result[i]["start"]["date"];
-      const uri = result[i]["uri"];
+		for (i = 0; i < result.length; i++) {
+			const displayName = result[i]['venue']['displayName'];
+			const location = result[i]['location']['city'];
+			const date = result[i]['start']['date'];
+			const uri = result[i]['uri'];
 
-      const eventDiv = $("<div class='event-div'>");
-      const eventName = $("<span class='event-name'>").html(displayName);
-      const eventLocation = $("<span class='event-location'>").html(location);
-      const spacer = $("<span class='spacer'>").html(" - ");
-      const eventStart = $("<span class='event-start'>").html(date);
-      const eventUri = $("<span class='event-uri'>").html(
-        "<a href=" + uri + ">Tickets</a>"
-      );
+			const eventDiv = $("<div class='event-div'>");
+			const eventName = $("<span class='event-name'>").html(displayName);
+			const eventLocation = $("<span class='event-location'>").html(location);
+			const spacer = $("<span class='spacer'>").html(' - ');
+			const eventStart = $("<span class='event-start'>").html(date);
+			const eventUri = $("<span class='event-uri'>").html('<a href=' + uri + '>Tickets</a>');
 
-      eventDiv.append(
-        eventStart,
-        // "<span class='spacer'> - </span>",
-        eventName,
-        // "<span class='spacer'> - </span>",
-        eventLocation,
-        // spacer,
-        eventUri
-      );
+			eventDiv.append(
+				eventStart,
+				// "<span class='spacer'> - </span>",
+				eventName,
+				// "<span class='spacer'> - </span>",
+				eventLocation,
+				// spacer,
+				eventUri
+			);
 
-      $("#events-div").append(eventDiv);
-      console.log(`${displayName} - ${location} ${date}, ${uri}`);
-    }
-  });
+			$('#events-div').append(eventDiv);
+			console.log(`${displayName} - ${location} ${date}, ${uri}`);
+		}
+	});
 };
 
-$(document).ready(function() {
-  $("#fullpage").fullpage({
-    //options here
-    // autoScrolling: true,
-    // scrollHorizontally: true,
-    css3: true,
-    continuousVertical: false
-    // autoScrolling: false
-    // afterRender: function() {
-    //   $("#header").css("opacity", 0);
-    // }
-    // afterLoad: function(index, nextIndex) {
-    //   if (index !== 0) {
-    //     $("#header").css("opacity", 1);
-    //   } else if (index === 0) {
-    //     $("#header").css("opacity", 0);
-    //   }
-    // }
-    // onLeave: function(index, nextIndex, direction) {
-    //   if (index === 0) {
-    //     $("#header").css("opacity", 0);
-    //   } else {
-    //     $("#header").css("opacity", 1);
-    //   }
-    // }
-  });
+$(document).ready(function () {
+	let filtered = 'false';
 
-  $(".store-carousel").slick({
-    slidesToShow: 3,
-    speed: 300,
-    infinite: true,
-    dots: true,
-    arrows: true,
-    adaptiveHeight: true,
-    responsive: [
-      {
-        breakpoint: 1440,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-  //methods
-  $.fn.fullpage.setAllowScrolling(true);
+	// $('.albums-only').on('click', function(){
+	// 	if (filtered ==false) {
+	// 		$('.')
+	// 	}
+	// });
 
-  var audio = $("audio")[0];
+	$('#fullpage').fullpage({
+		//options here
+		// anchors: [ 'landing', 'shows', 'store', 'about' ],
+		autoScrolling: false,
 
-  $("#play-pause").on("click", function() {
-    if (audio.paused) {
-      audio.play();
-      $("#play-pause").text("Pause");
-    } else {
-      audio.pause();
-      $("#play-pause").text("Listen");
-    }
-  });
+		// scrollHorizontally: true,
+		css3: true,
+		continuousVertical: false
+		// autoScrolling: false
+		// afterRender: function() {
+		//   $("#header").css("opacity", 0);
+		// }
+		// afterLoad: function(index, nextIndex) {
+		//   if (index !== 0) {
+		//     $("#header").css("opacity", 1);
+		//   } else if (index === 0) {
+		//     $("#header").css("opacity", 0);
+		//   }
+		// }
+		// onLeave: function(index, nextIndex, direction) {
+		//   if (index === 0) {
+		//     $("#header").css("opacity", 0);
+		//   } else {
+		//     $("#header").css("opacity", 1);
+		//   }
+		// }
+	});
 
-  var length = audio.duration;
-  var current_time = audio.currentTime;
-  audio.onTimeUpdate = function() {
-    $("#progress").css("width", (current_time / length) * 100 + "%");
-  };
+	$('.store-carousel').slick({
+		slidesToShow: 3,
+		speed: 300,
+		infinite: true,
+		dots: true,
+		arrows: true,
+		adaptiveHeight: true,
+		responsive: [{
+				breakpoint: 1440,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 700,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+	//methods
+	$.fn.fullpage.setAllowScrolling(true);
 
-  eventSearch();
+	var audio = $('audio')[0];
 
-  // $(window).scroll(function() {
-  //   if ($(this).scrollTop() > 100) {
-  //   } else {
-  //     $("#header").hide();
-  //   }
-  // });
+	$('#play-pause').on('click', function () {
+		if (audio.paused) {
+			audio.play();
+			$('#play-pause').text('Pause');
+		} else {
+			audio.pause();
+			$('#play-pause').text('Listen');
+		}
+	});
+
+	var length = audio.duration;
+	var current_time = audio.currentTime;
+	audio.onTimeUpdate = function () {
+		$('#progress').css('width', current_time / length * 100 + '%');
+	};
+
+	eventSearch();
+
+	// $(window).scroll(function() {
+	//   if ($(this).scrollTop() > 100) {
+	//   } else {
+	//     $("#header").hide();
+	//   }
+	// });
 });
