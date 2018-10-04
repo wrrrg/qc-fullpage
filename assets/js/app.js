6334bc9dbfafb720fd9ea9d151ec55b0815b235a
wrrrg <bill.gryta@gmail.com>
2018-10-04 10:04:28 -0500

    more show json

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,149 +1,199 @@
 // events ajax to songkick
-const songkick = 'io09K9l3ebJxmxe2';
-const skQuietCompanyId = '582111';
+const songkick = "io09K9l3ebJxmxe2";
+const skQuietCompanyId = "582111";
 
-eventSearch = function () {
-	$('#events-div').empty();
-	$.ajax({
-		url: 'https://api.songkick.com/api/3.0/artists/' + skQuietCompanyId + '/calendar.json?apikey=' + songkick,
-		method: 'GET'
-	}).then(function (res) {
-		console.log(res);
-		const result = res['resultsPage']['results']['event'];
-		console.log(result);
-		console.log(result.length);
+eventSearch = function() {
+  $("#events-div").empty();
+  $.ajax({
+    url:
+      "https://api.songkick.com/api/3.0/artists/" +
+      skQuietCompanyId +
+      "/calendar.json?apikey=" +
+      songkick,
+    method: "GET"
+  }).then(function(res) {
+    console.log(res);
+    const result = res["resultsPage"]["results"]["event"];
+    console.log(result);
+    console.log(result.length);
 
-		for (i = 0; i < result.length; i++) {
-			const displayName = result[i]['venue']['displayName'];
-			const location = result[i]['location']['city'];
-			const date = result[i]['start']['date'];
-			const uri = result[i]['uri'];
+    for (i = 0; i < result.length; i++) {
+      const displayName = result[i]["venue"]["displayName"];
+      const location = result[i]["location"]["city"];
+      const date = result[i]["start"]["date"];
+      const uri = result[i]["uri"];
 
-			const eventDiv = $("<div class='event-div'>");
-			const eventName = $("<span class='event-name'>").html(displayName);
-			const eventLocation = $("<span class='event-location'>").html(location);
-			const spacer = $("<span class='spacer'>").html(' - ');
-			const eventStart = $("<span class='event-start'>").html(date);
-			const eventUri = $("<span class='event-uri'>").html('<a href=' + uri + '>Tickets</a>');
+      const eventDiv = $("<div class='event-div'>");
+      const eventName = $("<span class='event-name'>").html(displayName);
+      const eventLocation = $("<span class='event-location'>").html(location);
+      const spacer = $("<span class='spacer'>").html(" - ");
+      const eventStart = $("<span class='event-start'>").html(date);
+      const eventUri = $("<span class='event-uri'>").html(
+        "<a href=" + uri + ">Tickets</a>"
+      );
 
-			eventDiv.append(
-				eventStart,
-				// "<span class='spacer'> - </span>",
-				eventName,
-				// "<span class='spacer'> - </span>",
-				eventLocation,
-				// spacer,
-				eventUri
-			);
+      eventDiv.append(
+        eventStart,
+        // "<span class='spacer'> - </span>",
+        eventName,
+        // "<span class='spacer'> - </span>",
+        eventLocation,
+        // spacer,
+        eventUri
+      );
 
-			$('#events-div').append(eventDiv);
-			console.log(`${displayName} - ${location} ${date}, ${uri}`);
-		}
-	});
+      $("#events-div").append(eventDiv);
+      console.log(`${displayName} - ${location} ${date}, ${uri}`);
+    }
+  });
 };
 
 eventFromJSON = function() {
-	
-}
+  $("#events-div").empty();
+  for (i = 0; i < showlist.length; i++) {
+    console.log(showlist[i]);
 
-$(document).ready(function () {
-	let filtered = 'false';
+    const venue = showlist[i]["venue"];
+    const location = showlist[i]["city"];
+    const date = showlist[i]["date"];
+    const tickets = showlist[i]["tickets"];
+    const facebook = showlist[i]["facebook"];
 
-	// $('.albums-only').on('click', function(){
-	// 	if (filtered ==false) {
-	// 		$('.')
-	// 	}
-	// });
+    const eventDiv = $("<div class='event-div'>");
+    const eventName = $("<span class='event-name'>").html(venue);
+    const eventLocation = $("<span class='event-location'>").html(location);
+    const spacer = $("<span class='spacer'>").html(" - ");
+    const eventStart = $("<span class='event-start'>").html(date);
+    let eventUri;
+    if (showlist[i]["freeShow"]) {
+      eventUri = $("<span class='event-uri'>").html(
+        "<a href=" + facebook + ">Free Show</a>"
+      );
+    } else {
+      eventUri = $("<span class='event-uri'>").html(
+        "<a href=" + tickets + ">Tickets</a>"
+      );
+    }
+    const eventFB = $("<span class='event-fb'>").html(
+      "<a href=" + facebook + ">FB</a>"
+    );
 
-	$('#fullpage').fullpage({
-		//options here
-		// anchors: [ 'landing', 'shows', 'store', 'about' ],
-		autoScrolling: false,
+    eventDiv.append(
+      eventStart,
+      // "<span class='spacer'> - </span>",
+      eventName,
+      // "<span class='spacer'> - </span>",
+      eventLocation,
+      // spacer,
+      eventUri,
+      //
+      eventFB
+    );
 
-		// scrollHorizontally: true,
-		css3: true,
-		continuousVertical: false
-		// autoScrolling: false
-		// afterRender: function() {
-		//   $("#header").css("opacity", 0);
-		// }
-		// afterLoad: function(index, nextIndex) {
-		//   if (index !== 0) {
-		//     $("#header").css("opacity", 1);
-		//   } else if (index === 0) {
-		//     $("#header").css("opacity", 0);
-		//   }
-		// }
-		// onLeave: function(index, nextIndex, direction) {
-		//   if (index === 0) {
-		//     $("#header").css("opacity", 0);
-		//   } else {
-		//     $("#header").css("opacity", 1);
-		//   }
-		// }
-	});
+    $("#events-div").append(eventDiv);
+  }
+};
 
-	$('.store-carousel').slick({
-		slidesToShow: 3,
-		speed: 300,
-		infinite: true,
-		dots: true,
-		arrows: true,
-		adaptiveHeight: true,
-		responsive: [{
-				breakpoint: 1440,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 2,
-					infinite: true,
-					dots: true
-				}
-			},
-			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 2,
-					infinite: true,
-					dots: true
-				}
-			},
-			{
-				breakpoint: 700,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			}
-		]
-	});
-	//methods
-	$.fn.fullpage.setAllowScrolling(true);
+$(document).ready(function() {
+  let filtered = "false";
 
-	var audio = $('audio')[0];
+  // $('.albums-only').on('click', function(){
+  // 	if (filtered ==false) {
+  // 		$('.')
+  // 	}
+  // });
 
-	$('#play-pause').on('click', function () {
-		if (audio.paused) {
-			audio.play();
-			$('#play-pause').text('Pause');
-		} else {
-			audio.pause();
-			$('#play-pause').text('Listen');
-		}
-	});
+  $("#fullpage").fullpage({
+    //options here
+    // anchors: [ 'landing', 'shows', 'store', 'about' ],
+    autoScrolling: false,
 
-	var length = audio.duration;
-	var current_time = audio.currentTime;
-	audio.onTimeUpdate = function () {
-		$('#progress').css('width', current_time / length * 100 + '%');
-	};
+    // scrollHorizontally: true,
+    css3: true,
+    continuousVertical: false
+    // autoScrolling: false
+    // afterRender: function() {
+    //   $("#header").css("opacity", 0);
+    // }
+    // afterLoad: function(index, nextIndex) {
+    //   if (index !== 0) {
+    //     $("#header").css("opacity", 1);
+    //   } else if (index === 0) {
+    //     $("#header").css("opacity", 0);
+    //   }
+    // }
+    // onLeave: function(index, nextIndex, direction) {
+    //   if (index === 0) {
+    //     $("#header").css("opacity", 0);
+    //   } else {
+    //     $("#header").css("opacity", 1);
+    //   }
+    // }
+  });
 
-	eventSearch();
+  $(".store-carousel").slick({
+    slidesToShow: 3,
+    speed: 300,
+    infinite: true,
+    dots: true,
+    arrows: true,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  //methods
+  $.fn.fullpage.setAllowScrolling(true);
 
-	// $(window).scroll(function() {
-	//   if ($(this).scrollTop() > 100) {
-	//   } else {
-	//     $("#header").hide();
-	//   }
-	// });
+  var audio = $("audio")[0];
+
+  $("#play-pause").on("click", function() {
+    if (audio.paused) {
+      audio.play();
+      $("#play-pause").text("Pause");
+    } else {
+      audio.pause();
+      $("#play-pause").text("Listen");
+    }
+  });
+
+  var length = audio.duration;
+  var current_time = audio.currentTime;
+  audio.onTimeUpdate = function() {
+    $("#progress").css("width", (current_time / length) * 100 + "%");
+  };
+
+  eventFromJSON();
+  //   eventSearch();
+
+  // $(window).scroll(function() {
+  //   if ($(this).scrollTop() > 100) {
+  //   } else {
+  //     $("#header").hide();
+  //   }
+  // });
 });
--- assets/js/shows.js
+++ assets/js/shows.js
@@ -1,51 +1,60 @@
-const shows = [{
-        "date": "10/18",
-        "venue": "Electric Church",
-        "city": "Austin, TX",
-        "tickets": "https://www.eventbrite.com/e/quiet-company-the-collection-fertility-house-tickets-50388342890?aff=efbeventtix",
-        "facebook": "https://www.facebook.com/events/294037488076205/"
-    },
-    {
-        "date": "10/12",
-        "venue": "Common Grounds",
-        "city": "Waco, TX",
-        "tickets": "https://www.ticketfly.com/purchase/event/1747687",
-        "facebook": "https://www.facebook.com/events/309050032983569/"
-    },
-    {
-        "date": "10/18",
-        "venue": "The Backyard",
-        "city": "Waco, TX",
-        "tickets": "https://www.stubwire.com/event/thetoadies/thebackyard/waco/18167/",
-        "facebook": ""
-    },
-    {
-        "date": "10/19",
-        "venue": "Brewster St. Ice House",
-        "city": "Corpus Christi, TX",
-        "tickets": "https://www.ticketfly.com/purchase/event/1738808?utm_medium=459899&_ga=2.246754436.1408372313.1533590287-130073232.1533590286",
-        "facebook": ""
-    },
-    {
-        "date": "10/20",
-        "venue": "Goldthwaite Music Festival",
-        "city": "Goldthwaite, TX",
-        "freeShow": true,
-        "tickets": "https://www.facebook.com/events/2077288542583514/",
-        "facebook": "https://www.facebook.com/events/2077288542583514/"
-    },
-    {
-        "date": "11/3",
-        "venue": "Club Dada",
-        "city": "Dallas, TX",
-        "tickets": "",
-        "facebook": "https://www.facebook.com/events/671733909867360/"
-    },
-    {
-        "date": "11/16",
-        "venue": "White Oak Music Hall",
-        "city": "Houston, TX",
-        "tickets": "",
-        "facebook": "https://www.facebook.com/events/1385426358257206/"
-    },
-]
+const showlist = [
+  {
+    date: "10/11",
+    venue: "Electric Church",
+    city: "Austin, TX",
+    freeShow: false,
+    tickets:
+      "https://www.eventbrite.com/e/quiet-company-the-collection-fertility-house-tickets-50388342890?aff=efbeventtix",
+    facebook: "https://www.facebook.com/events/294037488076205"
+  },
+  {
+    date: "10/12",
+    venue: "Common Grounds",
+    city: "Waco, TX",
+    freeShow: false,
+    tickets: "https://www.ticketfly.com/purchase/event/1747687",
+    facebook: "https://www.facebook.com/events/309050032983569"
+  },
+  {
+    date: "10/18",
+    venue: "The Backyard",
+    city: "Waco, TX",
+    freeShow: false,
+    tickets: "https://www.stubwire.com/event/thetoadies/thebackyard/waco/18167",
+    facebook: null
+  },
+  {
+    date: "10/19",
+    venue: "Brewster St. Ice House",
+    city: "Corpus Christi, TX",
+    freeShow: false,
+    tickets:
+      "https://www.ticketfly.com/purchase/event/1738808?utm_medium=459899&_ga=2.246754436.1408372313.1533590287-130073232.1533590286",
+    facebook: null
+  },
+  {
+    date: "10/20",
+    venue: "Goldthwaite Music Festival",
+    city: "Goldthwaite, TX",
+    freeShow: true,
+    tickets: "https://www.facebook.com/events/2077288542583514/",
+    facebook: "https://www.facebook.com/events/2077288542583514/"
+  },
+  {
+    date: "11/3",
+    venue: "Club Dada",
+    city: "Dallas, TX",
+    freeShow: false,
+    tickets: null,
+    facebook: "https://www.facebook.com/events/671733909867360/"
+  },
+  {
+    date: "11/16",
+    venue: "White Oak Music Hall",
+    city: "Houston, TX",
+    freeShow: false,
+    tickets: null,
+    facebook: "https://www.facebook.com/events/1385426358257206/"
+  }
+];
