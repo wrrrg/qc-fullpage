51d9158aa7ec55e0a10eade7e97b08ef8062ed1d
BillMac <bill.gryta@gmail.com>
2018-11-02 19:27:22 -0500

    new store and song

--- assets/js/shows.js
+++ assets/js/shows.js
@@ -1,53 +1,52 @@
 const showlist = [
-  {
-    date: "10/11",
-    venue: "Electric Church",
-    city: "Austin, TX",
-    freeShow: false,
-    tickets:
-      "https://www.eventbrite.com/e/quiet-company-the-collection-fertility-house-tickets-50388342890?aff=efbeventtix",
-    facebook: "https://www.facebook.com/events/294037488076205"
-  },
-  {
-    date: "10/12",
-    venue: "Common Grounds",
-    city: "Waco, TX",
-    freeShow: false,
-    tickets: "https://www.ticketfly.com/purchase/event/1747687",
-    facebook: "https://www.facebook.com/events/309050032983569"
-  },
-  {
-    date: "10/18",
-    venue: "The Backyard",
-    city: "Waco, TX",
-    freeShow: false,
-    tickets: "https://www.stubwire.com/event/thetoadies/thebackyard/waco/18167",
-    facebook: null
-  },
-  {
-    date: "10/19",
-    venue: "Brewster St. Ice House",
-    city: "Corpus Christi, TX",
-    freeShow: false,
-    tickets:
-      "https://www.ticketfly.com/purchase/event/1738808?utm_medium=459899&_ga=2.246754436.1408372313.1533590287-130073232.1533590286",
-    facebook: null
-  },
-  {
-    date: "10/20",
-    venue: "Goldthwaite Music Festival",
-    city: "Goldthwaite, TX",
-    freeShow: true,
-    tickets: "https://www.facebook.com/events/2077288542583514",
-    facebook: "https://www.facebook.com/events/2077288542583514"
-  },
+  // {
+  //   date: "10/11",
+  //   venue: "Electric Church",
+  //   city: "Austin, TX",
+  //   freeShow: false,
+  //   tickets:
+  //     "https://www.eventbrite.com/e/quiet-company-the-collection-fertility-house-tickets-50388342890?aff=efbeventtix",
+  //   facebook: "https://www.facebook.com/events/294037488076205"
+  // },
+  // {
+  //   date: "10/12",
+  //   venue: "Common Grounds",
+  //   city: "Waco, TX",
+  //   freeShow: false,
+  //   tickets: "https://www.ticketfly.com/purchase/event/1747687",
+  //   facebook: "https://www.facebook.com/events/309050032983569"
+  // },
+  // {
+  //   date: "10/18",
+  //   venue: "The Backyard",
+  //   city: "Waco, TX",
+  //   freeShow: false,
+  //   tickets: "https://www.stubwire.com/event/thetoadies/thebackyard/waco/18167",
+  //   facebook: null
+  // },
+  // {
+  //   date: "10/19",
+  //   venue: "Brewster St. Ice House",
+  //   city: "Corpus Christi, TX",
+  //   freeShow: false,
+  //   tickets:
+  //     "https://www.ticketfly.com/purchase/event/1738808?utm_medium=459899&_ga=2.246754436.1408372313.1533590287-130073232.1533590286",
+  //   facebook: null
+  // },
+  // {
+  //   date: "10/20",
+  //   venue: "Goldthwaite Music Festival",
+  //   city: "Goldthwaite, TX",
+  //   freeShow: true,
+  //   tickets: "https://www.facebook.com/events/2077288542583514",
+  //   facebook: "https://www.facebook.com/events/2077288542583514"
+  // },
   {
     date: "11/3",
     venue: "Club Dada",
     city: "Dallas, TX",
     freeShow: false,
-    tickets:
-      "https://www.eventbrite.com/e/mo-lowda-the-humble-quiet-company-club-dada-tickets-46988752624",
+    tickets: "https://www.eventbrite.com/e/mo-lowda-the-humble-quiet-company-club-dada-tickets-46988752624",
     facebook: "https://www.facebook.com/events/671733909867360"
   },
   {
@@ -55,8 +54,39 @@ const showlist = [
     venue: "White Oak Music Hall",
     city: "Houston, TX",
     freeShow: false,
-    tickets:
-      "https://wl.seetickets.us/event/The-Wheel-Workers-Quiet-Company-A-Sundae-Drive/368022?afflky=WhiteOakMusicHall",
+    tickets: "https://wl.seetickets.us/event/The-Wheel-Workers-Quiet-Company-A-Sundae-Drive/368022?afflky=WhiteOakMusicHall",
     facebook: "https://www.facebook.com/events/1385426358257206"
-  }
+  },
+  {
+    date: "11/18",
+    venue: "Southern Heights Brewery",
+    city: "Austin, TX",
+    freeShow: true,
+    tickets: "",
+    facebook: ""
+  },
+  {
+    date: "12/8",
+    venue: "Christmas House Show!",
+    city: "Houston, TX",
+    freeShow: false,
+    tickets: "https://quietcompany.bigcartel.com/product/houston-holiday-house-show-on-dec-8th",
+    facebook: ""
+  },
+  {
+    date: "12/14",
+    venue: "Christmas House Show!",
+    city: "Dallas, TX",
+    freeShow: false,
+    tickets: "https://quietcompany.bigcartel.com/product/dallas-holiday-house-show-on-dec-15th",
+    facebook: ""
+  },
+  {
+    date: "12/15",
+    venue: "Christmas House Show!",
+    city: "Austin, TX",
+    freeShow: false,
+    tickets: "https://quietcompany.bigcartel.com/product/austin-holiday-house-show-on-dec-15th",
+    facebook: ""
+  },
 ];
